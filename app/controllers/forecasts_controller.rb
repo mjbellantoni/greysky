3c9e00b08242458f534c2895c16322806df89d4a
Matthew Bellantoni <mjbellantoni@gmail.com>
2024-03-15 17:36:52 -0400

    Remove superfluous controller instance variables

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -4,15 +4,9 @@ class ForecastsController < ApplicationController
     # API needs a lat/lon pair to look up the forecast.
     results = Geokit::Geocoders::GeocodioGeocoder.geocode(params[:zip_code])
     lat, lon = results.lat, results.lng
-    @city = results.city
+    city = results.city
 
-    @forecast = Forecast.new(zip_code: params[:zip_code], city: @city)
+    @forecast = Forecast.new(zip_code: params[:zip_code], city: city)
     @forecast.fetch_from_nws(lat, lon)
-
-    @current_temp = @forecast.current_temp
-    @current_weather = @forecast.current_weather
-    @forecast_periods = @forecast.forecast_periods
-    @high_temp = @forecast.high_temp
-    @low_temp = @forecast.low_temp
   end
 end
