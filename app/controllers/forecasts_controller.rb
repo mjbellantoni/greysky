1bc3ac7eac6d69af8f6a37550f289eea484dfd59
Matthew Bellantoni <mjbellantoni@gmail.com>
2024-03-15 16:59:31 -0400

    Geocode the ZIP code that's passed in
    This was hard coded from the spike when this code was created.

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -2,7 +2,7 @@ class ForecastsController < ApplicationController
   def show
     # Use Geokit to geocode the zipcode via the Geocodio API. The NWS
     # API needs a lat/lon pair to look up the forecast.
-    results = Geokit::Geocoders::GeocodioGeocoder.geocode("02134")
+    results = Geokit::Geocoders::GeocodioGeocoder.geocode(params[:zip_code])
     lat, lon = results.lat, results.lng
     @city = results.city
 
