514bb85845a04d325b859b7deec0a93a1edf85b3
Matthew Bellantoni <mjbellantoni@gmail.com>
2024-03-16 15:36:07 -0400

    Replace the other call to Geokit with the wrapped version

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,11 +1,9 @@
 class ForecastsController < ApplicationController
   def show
-    # Use Geokit to geocode the zipcode via the Geocodio API. The NWS
-    # API needs a lat/lon pair to look up the forecast.
-    results = Geokit::Geocoders::GeocodioGeocoder.geocode(params[:zip_code])
-    lat, lon = results.lat, results.lng
-    city = results.city
-
-    @forecast = Forecast.fetch(params[:zip_code], lat, lon, city)
+    query = Query.new(q: params[:zip_code])
+    # Use Geocoder to geocode the ZIP code. The NWS API needs a lat/lng
+    # pair to look up the forecast.
+    result = Geocoder.geocode(query)
+    @forecast = Forecast.fetch(result.zip_code, result.lat, result.lng, result.city)
   end
 end
